e3d36e2e71f90d545ca3f7e7865f473af98dbc32
Tauhid hossen <tauhidhossen04@gmail.com>
2024-07-08 18:29:14 +0600

    Product section created with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import Navber from './Components/Navber/Navber'
 import Hero from './Components/Hero/Hero'
+import Product from './Components/Products/Product'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
+import React from 'react';
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
     <div>
       <Navber></Navber>
       <Hero></Hero>
+      <Product></Product>
     </div>
   )
 }
